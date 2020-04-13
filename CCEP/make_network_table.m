e8520a6f8bbb723dc603b522abb7f3391346438a
Sifra Blok <sifra@bird.umcutrecht.nl>
2020-04-13 13:41:47 +0200

    d

--- CCEP/make_network_table.m
+++ CCEP/make_network_table.m
@@ -1,16 +1,13 @@
-%% pre-allocation
-
-clear 
-
-config_CCEP
 
 %% set paths
+clear 
 
 %myDataPath = setLocalDataPath(cfg);
 myDataPath.CCEPpath = '/Fridge/users/sifra/derivatives/CCEP/' ;
 myDataPath.dataPath = '/Fridge/chronic_ECoG/';
 
 % set paths
+addpath(genpath('/home/sifra/git_repositories/CCEP_NMM_SB/CCEP/'));
 addpath(genpath('/home/sifra/git_repositories/eeglab/')) ;    
 addpath('/home/sifra/git_repositories/fieldtrip');
 ft_defaults
@@ -18,6 +15,8 @@ ft_defaults
 localDataPath.CCEPpath = '/Fridge/users/sifra/derivatives/CCEP/'; % /Fridge/users/sifra/derivatives/CCEP
 localDataPath.dataPath = '/Fridge/chronic_ECoG/';
 
+%% pre-allocation
+config_CCEP
 
 %% choose CCEP file
 files = dir(fullfile(localDataPath.CCEPpath,cfg.sub_labels{1}, cfg.ses_label,'run-*',...
@@ -37,23 +36,18 @@ if length(runs) > 1
     
     for n = 1:(length(stimnames)-1)
         if length(stimnames{n,1}) ~= length(stimnames{(n+1),1})
-             % Find the names of the extra stimulation pairs
+            
+            % Find the names of the extra stimulation pairs
             diff_stimchan = setdiff(stimnames{n,1},stimnames{n+1,1});
             index = find(ismember(runs(n).ccep.stimpnames, diff_stimchan));     
-
-           % index(n,:) = find(ismember(runs(n).ccep.stimpnames, diff_stimchan)); % deze bestaat niet voor de kleinste array
-
-            % Find which columns found in diff_stimchan. 
-            
-           
+             
+            % Find which columns found in diff_stimchan.            
            if size(runs(n).ccep.n1_peak_sample,2) > index(1)
-            
-               % if ~isempty (runs(n).ccep.n1_peak_sample(:,index))          % deze bestaat niet voor de kleinste array
-                  for i = index(end) :-1: index(1)
-                    runs(n).ccep.n1_peak_sample(:,i) = [];
-                    runs(n).ccep.n1_peak_amplitude(:,i) = [];
-                    fprintf('WARNING: Extra stimpairs of %s_%s are removed.', cfg.sub_labels{:}, cfg.ses_label);
-                  end 
+              for i = index(end) :-1: index(1)
+                   runs(n).ccep.n1_peak_sample(:,i) = [];
+                   runs(n).ccep.n1_peak_amplitude(:,i) = [];
+                   fprintf('WARNING: Extra stimpairs of %s_%s are removed.', cfg.sub_labels{:}, cfg.ses_label);
+               end 
            end 
         end
      end  
