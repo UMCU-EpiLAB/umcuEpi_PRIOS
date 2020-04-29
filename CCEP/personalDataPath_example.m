7d03732f1615423371e59fc487291bb54cf8c7d1
Dorien van Blooijs <45383793+dvanblooijs@users.noreply.github.com>
2020-04-29 08:15:13 +0200

    Update personalDataPath_example.m

--- CCEP/personalDataPath_example.m
+++ CCEP/personalDataPath_example.m
@@ -6,31 +6,13 @@ function localDataPath = personalDataPath_example(varargin)
 
 % function that contains local data path, is ignored in .gitignore
 
-if ~isempty(varargin{1})
-    if isstruct(varargin{1})
-        if sum(contains(fieldnames(varargin{1}),'sub_labels'))
-            if contains(varargin{1}.sub_labels,'REC2Stim')
-                % REC2Stim study
-                localDataPath.CCEPpath = '/XX/';
-                localDataPath.dataPath = '/XX/';
-            elseif contains(varargin{1}.sub_labels,'RESP')
-                % RESPect database
-                localDataPath.CCEPpath = '/XX/CCEP/';
-                localDataPath.dataPath = '/XX/CCEP/';
-            end
-        end
-    end
-end
-
+localDataPath.input = 'bla/CCEP/';
+localDataPath.output = '/bla/users/dorien/';
 
 % % set paths
-addpath(genpath('/XX/BasicCode_ECoG_DvB'))
-addpath(genpath('/XX/eeglab/'))
-addpath(genpath('/XX/CCEP_NMM'))
-
-fieldtrip_folder  = '/XX/fieldtrip/';
+fieldtrip_folder  = '/bla/fieldtrip/';
 % copy the private folder in fieldtrip to somewhere else
-fieldtrip_private = '/XX/fieldtrip_private/';
+fieldtrip_private = '/bla/fieldtrip_private/';
 addpath(fieldtrip_folder)
 addpath(fieldtrip_private)
 ft_defaults
