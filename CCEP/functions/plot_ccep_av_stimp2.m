cebb47e1512560f85984aaf6688acf9981f9b19f
Sifra Blok <sifra@bird.umcutrecht.nl>
2020-06-23 17:56:39 +0200

    plot ERs of 10 stims in figure

--- CCEP/functions/plot_ccep_av_stimp2.m
+++ CCEP/functions/plot_ccep_av_stimp2.m
@@ -15,12 +15,13 @@ tt = dataBase.tt;
         
         Stimpnm = stimchans{stimp};
         figure('Position',[1400 0 700 700])
-        hold on
+        %hold on
         xlim([-.2 1.5])
         ylabel('Average per electrodes (mV)')
         xlabel('time (s)') 
                 
          counter = 0;
+         counter_10 = 0;
          nmbr_of_ones = TotOnesStim(stimp);
          name = cell(1,nmbr_of_ones);
             
@@ -30,54 +31,44 @@ tt = dataBase.tt;
              
             for i = 1:size(LocaOnes)     
                 if ismember({Stimpnm}, LocaOnes{i,1}) && ismember(dataBase.ch{elec}, LocaOnes(i,2)) 
-                     counter = counter+1;
-                     name(:,counter) =  [LocaOnes(i,2)];                 % names of electrodes with a 1 on stimulation pair stimp.
-%                 end
-%             end
-%         end
-%         
-%         
-         for nmr_plot = 1:nmbr_of_ones                            % total number of ones for stimulation pair stimp  
-%             Electodes = [dataBase.ch(:)];
-%         
-%             loc_electr = all(ismember(Electodes, name{nmr_plot}),2 ) ; 
-%             loc_elect(:,nmr_plot) = find(loc_electr) ;              % Number of the location of the electrode with a 1 on stimpair stimp.
-         
-                ccep_plot = squeeze(dataBase.cc_epoch_sorted_avg(elec,stimp,:));
-                ccep_plot2 = squeeze(dataBase2.cc_epoch_sorted_avg(elec,stimp,:));       % stimulations of the 2 stims
-                ccep_plot(tt>-0.010 & tt<0.010) = NaN;
-                ccep_plot2(tt>-0.010 & tt<0.010) = NaN;
+                    counter = counter+1;
+                    name(:,counter) =  [LocaOnes(i,2)];                 % names of electrodes with a 1 on stimulation pair stimp.
+                     
+                    ccep_plot(:,counter) = squeeze(dataBase.cc_epoch_sorted_avg(elec,stimp,:));
+                    ccep_plot2(:,counter) = squeeze(dataBase2.cc_epoch_sorted_avg(elec,stimp,:));       % stimulations of the 2 stims
+                    ccep_plot(tt>-0.010 & tt<0.010) = NaN;
+                    ccep_plot2(tt>-0.010 & tt<0.010) = NaN;
+                end
+                                   
+                    subplot(2,1,1);
+                    plot(tt,  ccep_plot + [0:500:nmbr_of_ones*500-1]);   
+                    str = sprintf('Stimulation pair %s for 10 stimps', stimchans{stimp,1});
+                    title(str)
+                    xlim([-.2 1.5])
+                    ylabel('Average per electrodes (mV)')
+                    xlabel('time (s)') 
+                    set(gca,'YTick',500*(1:nmbr_of_ones),'YTickLabel',name(:)) ;
+                    hold  on
 
-                subplot(2,1,1);
-                plot(tt, nmr_plot*500+ccep_plot, 'LineWidth', 2);             % plot(tt, loc_elect*500+ccep_plot, 'k', 'LineWidth', 3);  
-                str = sprintf('Stimulation pair %s for 10 stimps', stimchans{stimp,1});
-                title(str)
-                xlim([-.2 1.5])
-                ylabel('Average per electrodes (mV)')
-                xlabel('time (s)') 
-                set(gca,'YTick',500*(1:counter),'YTickLabel',name(:)) ;
-                hold on 
-                
-                
-                subplot(2,1,2);
-                plot(tt, nmr_plot*500+ccep_plot2, 'LineWidth', 2); 
-                str2 = sprintf('Stimulation pair %s for 2 stimps', stimchans{stimp,1});
-                title(str2)
-                xlim([-.2 1.5])
-                ylabel('Average per electrodes (mV)')
-                xlabel('time (s)') 
-                set(gca,'YTick',500*(1:counter),'YTickLabel',name(:)) ;
-                hold on
-
+                    subplot(2,1,2);
+                    plot(tt,  ccep_plot2 + [0:500:nmbr_of_ones*500-1]);   
+                    str2 = sprintf('Stimulation pair %s for 2 stimps', stimchans{stimp,1});
+                    title(str2)
+                    xlim([-.2 1.5])
+                    ylabel('Average per electrodes (mV)')
+                    xlabel('time (s)') 
+                    set(gca,'YTick',500*(1:counter),'YTickLabel',name(:)) ;
+                    hold on
+                 
                  for j = 1:length(ER_in10st)
                     if ismember({Stimpnm}, ER_in10st{j,1}) && ismember(dataBase.ch{elec}, ER_in10st(j,2))   
-                        %% !!!! Naam wordt nu telkens overschreven als er meerdere zijn ER in 10 stims
-                        str_main = sprintf('ER in 10 stims. Stimulation pair %s for electrode %s', Stimpnm, elecnm);        
+                        %ERin10stim = size(find(ER_in10(:,1)==stimp),1);           % Number of 1's evoked by 10 stims for this stimulation pair
+                        counter_10 = counter_10+1;
+                        NameER(:,counter_10) =  [ER_in10st(j,2)];
+                        str_main = sprintf('10 stims evoke ER in %s,%s,%s,%s,%,%s,%s,%s',NameER{:});        
                         sgtitle(str_main)
                     end
                  end
-         end
-           
         end
        
      hold off
