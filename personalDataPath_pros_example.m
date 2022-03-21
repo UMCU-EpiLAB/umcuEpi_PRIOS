b6eff930183b6609c612d1f48cd1402a01066e6e
Sifra Blok <60007056+SiefBlok@users.noreply.github.com>
2022-03-21 15:55:16 +0100

    Update personalDataPath_pros_example.m

--- personalDataPath_pros_example.m
+++ personalDataPath_pros_example.m
@@ -1,3 +1,7 @@
+
+%% THIS IS AN EXAMPLE FILE
+% copy this one and fill in your own
+
 function localDataPath = personalDataPath_pros_example(varargin)
 
 % function that contains local data path, is ignored in .gitignore
@@ -8,14 +12,19 @@ localDataPath.CCEP_allpat = '/folder/to/location/to/save/derivatives/CCEP_files_
 localDataPath.CCEP_interObVar = '/folder/to/location/with/interobserver/data/';
 
 % set paths
-addpath(genpath('/folder/to/gitrepository/eeglab/'))     
-addpath('/folder/to/gitrepository/fieldtrip')
+fieldtrip_folder  = '/folder/to/gitrepository/fieldtrip/';
+fieldtrip_private = '/folder/to/gitrepository/fieldtrip_private/';
+jsonlab_folder    = '/folder/to/gitrepository/jsonlab/';
+violin_folder     = '/folder/to/gitrepository/external/Violinplot-Matlab/';
+addpath(violin_folder)
+addpath(fieldtrip_folder)
+addpath(fieldtrip_private)
+addpath(jsonlab_folder)
 ft_defaults
 
 % Remove paths (remove retrospective folder because filenames are matching)
 warning('off','all');
-rmpath(genpath('/folder/to/gitrepository/CCEP_NMM/CCEP/Retrospective_analysis/'))
-rmpath(genpath('/folder/to/gitrepository/fieldtrip/external/signal')) % to avoid usage of fieldtrip butter and filtfilt instead matlab functions
+rmpath(genpath('/folder/to/gitrepository/external/signal')) % to avoid usage of fieldtrip butter and filtfilt instead matlab functions
 warning('on','all');
 
 end
